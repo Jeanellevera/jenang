d377ae1d2b77b308d6fb044a1626168bd10a5c30
Jeanellevera <145831198+Jeanellevera@users.noreply.github.com>
2024-11-19 14:46:40 +0800

    config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'jean');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123456789');
 define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
